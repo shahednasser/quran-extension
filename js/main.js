4f11600e7843fa13c20a89afbb531fae3abffe83
unknown <shahednasser@gmail.com>
2019-02-23 11:40:29 +0200

    added reload button

--- js/main.js
+++ js/main.js
@@ -4,90 +4,10 @@
 
 $(document).ready(function(){
   let audio;
-  chrome.storage.local.get(['image', 'verse'], function(result){
-    chrome.storage.sync.get(['show_translation', 'translation_language', 'recitation',
-                                'translation_identifier'], function(syncResult){
-      if(navigator.onLine){
-        if(!syncResult.hasOwnProperty('show_translation') || !syncResult.hasOwnProperty('translation_language') ||
-            !syncResult.show_translation || !syncResult.translation_language || !syncResult.translation_identifier){
-              $(".translation-container").remove();
-            }
-        let now = (new Date()).getTime();
-        if(result.hasOwnProperty('image') && result.image && now <= result.image.timeout){
-          setBackgroundImage(result.image.src);
-        }
-        else {
-          let xhr = new XMLHttpRequest();
+  load(false);
 
-          $.ajax({
-            method: 'GET',
-            url: 'https://source.unsplash.com/1600x900/?nature,mountains,landscape',
-            headers: {
-              'Access-Control-Expose-Headers': 'ETag'
-            },
-            xhr: function() {
-             return xhr;
-            },
-            success: function(data){
-              setBackgroundImage(xhr.responseURL);
-              let timeout = calculateTimeout();
-              chrome.storage.local.set({image: {src: xhr.responseURL, timeout}});
-            },
-            error: function(){
-              setBackgroundImage('/assets/offline-image.jpg');
-            }
-          });
-        }
-
-        if(result.hasOwnProperty('verse') && result.verse && now <= result.verse.timeout){
-          setVerse(result.verse.data);
-          setTranslation(result.verse.translation);
-        }
-        else {
-          let verseNumber = Math.floor(Math.random() * 6236) + 1;
-          let url = 'http://api.alquran.cloud/v1/ayah/' + verseNumber + '/editions/quran-uthmani,';
-          if(syncResult.hasOwnProperty('recitation')){
-            url += syncResult.recitation;
-          } else {
-            url += 'ar.alafasy';
-          }
-          if(syncResult.hasOwnProperty('show_translation') && syncResult.show_translation &&
-            syncResult.hasOwnProperty('translation_identifier') && syncResult.translation_identifier &&
-            syncResult.translation_identifier){
-            url += "," + syncResult.translation_identifier;
-          }
-          $.get(url, function(data){
-            if(data.data){
-              let verse = {};
-              for(let i = 0; i < data.data.length; i++){
-                if(data.data[i].hasOwnProperty('audio')){
-                  verse.audio = data.data[i].audio;
-                } else if(data.data[i].edition.language === "ar"){
-                  setVerse(data.data[i]);
-                  verse.data = data.data[i];
-                } else {
-                  verse.translation = data.data[i];
-                  setTranslation(data.data[i]);
-                }
-              }
-              let timeout = calculateTimeout();
-              verse.timeout = timeout;
-              chrome.storage.local.set({verse});
-            }
-          }).fail(function(){
-            $(".translation-container").remove();
-            setVerse(getDefaultVerse());
-            $(".audio-player").remove();
-          });
-        }
-      }
-      else{
-        $(".translation-container").remove();
-        setBackgroundImage('assets/offline-image.jpg');
-        setVerse(getDefaultVerse());
-        $(".audio-player").remove();
-      }
-    });
+  $(".reload").click(function(){
+    load(true);
   });
 
   $(".audio-player").click(function(){
@@ -144,11 +64,115 @@ $(document).ready(function(){
     }
   })
 
+  function load(reload){
+    $(".reload img").hide();
+    $(".reload .loader").show();
+    chrome.storage.local.get(['image', 'verse'], function(result){
+      chrome.storage.sync.get(['show_translation', 'translation_language', 'recitation',
+                                  'translation_identifier'], function(syncResult){
+        if(navigator.onLine){
+          if(!syncResult.hasOwnProperty('show_translation') || !syncResult.hasOwnProperty('translation_language') ||
+              !syncResult.show_translation || !syncResult.translation_language || !syncResult.translation_identifier){
+                $(".translation-container").remove();
+              }
+          let now = (new Date()).getTime();
+          if(result.hasOwnProperty('image') && result.image && now <= result.image.timeout && !reload){
+            setBackgroundImage(result.image.src);
+          }
+          else {
+            let xhr = new XMLHttpRequest();
+
+            $.ajax({
+              method: 'GET',
+              url: 'https://source.unsplash.com/1600x900/?nature,mountains,landscape',
+              headers: {
+                'Access-Control-Expose-Headers': 'ETag'
+              },
+              xhr: function() {
+               return xhr;
+              },
+              success: function(data){
+                setBackgroundImage(xhr.responseURL);
+                let timeout = calculateTimeout();
+                chrome.storage.local.set({image: {src: xhr.responseURL, timeout}});
+              },
+              error: function(){
+                setBackgroundImage('/assets/offline-image.jpg');
+              },
+              complete: function(){
+                if(reload){
+                  $(".reload img").show();
+                  $(".reload .loader").hide();
+                }
+              }
+            });
+          }
+
+          if(result.hasOwnProperty('verse') && result.verse && now <= result.verse.timeout && !reload){
+            setVerse(result.verse.data);
+            if(syncResult.hasOwnProperty('show_translation') && syncResult.show_translation &&
+              syncResult.hasOwnProperty('translation_identifier') && syncResult.translation_identifier &&
+              syncResult.translation_identifier){
+              setTranslation(result.verse.translation);
+            }
+          }
+          else {
+            let verseNumber = Math.floor(Math.random() * 6236) + 1;
+            let url = 'http://api.alquran.cloud/v1/ayah/' + verseNumber + '/editions/quran-uthmani,';
+            if(syncResult.hasOwnProperty('recitation')){
+              url += syncResult.recitation;
+            } else {
+              url += 'ar.alafasy';
+            }
+            if(syncResult.hasOwnProperty('show_translation') && syncResult.show_translation &&
+              syncResult.hasOwnProperty('translation_identifier') && syncResult.translation_identifier &&
+              syncResult.translation_identifier){
+              url += "," + syncResult.translation_identifier;
+            }
+            $.get(url, function(data){
+              if(data.data){
+                let verse = {};
+                for(let i = 0; i < data.data.length; i++){
+                  if(data.data[i].hasOwnProperty('audio')){
+                    verse.audio = data.data[i].audio;
+                  } else if(data.data[i].edition.language === "ar"){
+                    setVerse(data.data[i]);
+                    verse.data = data.data[i];
+                  } else {
+                    verse.translation = data.data[i];
+                    setTranslation(data.data[i]);
+                  }
+                }
+                let timeout = calculateTimeout();
+                verse.timeout = timeout;
+                chrome.storage.local.set({verse});
+              }
+            }).fail(function(){
+              $(".translation-container").remove();
+              setVerse(getDefaultVerse());
+              $(".audio-player").remove();
+            });
+          }
+        }
+        else{
+          $(".translation-container").remove();
+          setBackgroundImage('assets/offline-image.jpg');
+          setVerse(getDefaultVerse());
+          $(".audio-player").remove();
+        }
+      });
+    });
+  }
+
   function setBackgroundImage(url){
     $(".background-image").attr('src', url).on('load', function(){
       $(this).animate({opacity: 1}, 500);
+      $(".reload img").show();
+      $(".reload .loader").hide();
     }).on('error', function(){
       $(this).attr('src', 'assets/offline-image.jpg').animate({opacity: 1}, 500);
+      $(".reload img").show();
+      $(".reload .loader").hide();
     });
   }
 
